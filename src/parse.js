343cf404bd66f8be1202bd1a7ef2d55fac1d650e
vlad1slove1 <vlad5114@gmail.com>
2023-01-30 12:01:35 +0300

    fix parse.js with try.catch

--- src/parse.js
+++ src/parse.js
@@ -1,26 +1,26 @@
 export default (content) => {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(content, 'text/xml');
+  try {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(content, 'text/xml');
 
-  const parsingError = doc.querySelector('parsererror');
-  if (parsingError) {
-    const error = new Error();
-    error.type = 'parsingError';
-    throw error;
+    const feed = {
+      title: doc.querySelector('title').textContent,
+      description: doc.querySelector('description').textContent,
+      link: doc.querySelector('link').textContent,
+    };
+
+    const feedItems = doc.querySelectorAll('item');
+    const posts = [...feedItems].map((item) => ({
+      title: item.querySelector('title').textContent,
+      description: item.querySelector('description').textContent,
+      link: item.querySelector('link').textContent,
+    }));
+
+    return { feed, posts };
+  } catch (error) {
+    const err = new Error();
+    err.name = 'parsingError';
+
+    throw err;
   }
-
-  const feed = {
-    title: doc.querySelector('title').textContent,
-    description: doc.querySelector('description').textContent,
-    link: doc.querySelector('link').textContent,
-  };
-
-  const feedItems = doc.querySelectorAll('item');
-  const posts = [...feedItems].map((item) => ({
-    title: item.querySelector('title').textContent,
-    description: item.querySelector('description').textContent,
-    link: item.querySelector('link').textContent,
-  }));
-
-  return { feed, posts };
 };
